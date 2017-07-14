515394b8bf0550a8962075147ad6984472af3a23
Rajesh W <wraejesh@gmail.com>
2017-07-14 21:43:36 +0530

    Added Next button

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ class App extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            data: '',
+            data: {},
             index: 0,
         }
     }
@@ -16,23 +16,33 @@ class App extends Component {
                 this.setState({data: json});
             });
     }
-
+    handleClick = (e) => {
+        console.log(e.target, this); // eslint-disable-line
+        let {index : indexCounter } = this.state;
+        this.setState({index: indexCounter+1})
+    }
     render() {
         const { data, index } = this.state;
-        const item = data.quiz[index];
+        const item = Object.keys(data).length !== 0 ? data.quiz[index] : {};
         return (
             <div className="App" >
                 <div className="App-header">
                     <h2> Quizz </h2>
                 </div>
-                { item
+                { Object.keys(item).length !== 0
                 ?   <div>
+                    {console.log(item) }
                         <div className="App-intro">
                             { item.question }
                         </div>
                         <div>
-                            { item.options }
+                            {item.options.map((element) =>
+                                <div key={element}>
+                                    <input type="radio" name="options"/>{element}
+                                </div>
+                            )}
                         </div>
+                        <button onClick={this.handleClick}>Next</button>
                      </div>
                 : <img src="bgLoad.gif" alt="loading" />
                 }
